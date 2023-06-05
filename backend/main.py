d731ced3432541338353ef6d2710f9d67eede0f1
byeonkho <byeon.kho@gmail.com>
2023-06-05 14:48:20 +0800

    setup backend db connection, basic GET users function

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,151 @@
-# This is a sample Python script.
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import pymysql
+from datetime import datetime
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+pymysql.install_as_MySQLdb()
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'mysql://user:2r4u7udlol@localhost:3306/sys'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    user_firstname = db.Column(db.String(80), nullable=False)
+    user_lastname = db.Column(db.String(80), nullable=False)
+    user_password = db.Column(db.String(80), nullable=False)
+    user_is_admin = db.Column(db.Boolean, default=True, nullable=False)
+
+    def __init__(self, username, user_firstname, user_lastname,
+                 user_password):
+        self.username = username
+        self.user_firstname = user_firstname
+        self.user_password = user_password
+        self.user_lastname = user_lastname
+
+class UserWeight(db.Model):
+    weight_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+    weight = db.Column(db.Float, nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    user = db.relationship('User', backref=db.backref('weights', lazy=True))
+
+    def __init__(self, user_id, weight):
+        self.user_id = user_id
+        self.weight = weight
+
+class UserNutrition(db.Model):
+    nutrition_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+    ingredient_id = db.Column(db.Integer, nullable=False)
+    date_entered = db.Column(db.DateTime, nullable=False,
+                            default=datetime.utcnow)
+
+    # nutrition
+    calories = db.Column(db.String(80), default="0")
+    fat = db.Column(db.String(80), default="0")
+    saturated = db.Column(db.String(80) , default="0")
+    polyunsaturatedfat = db.Column(db.String(80), default="0")
+    monounsaturatedfat = db.Column(db.String(80), default="0")
+    transfat = db.Column(db.String(80), default="0")
+    cholesterol = db.Column(db.String(80), default="0")
+    sodium = db.Column(db.String(80), default="0")
+    potassium = db.Column(db.String(80), default="0")
+    carbohydrates = db.Column(db.String(80), default="0")
+    fiber = db.Column(db.String(80), default="0")
+    sugar = db.Column(db.String(80), default="0")
+    protein = db.Column(db.String(80), default="0")
+
+    user = db.relationship('User', backref=db.backref('calories', lazy=True))
+
+    def __init__(self, user_id, date_entered, calories, ingredient_id):
+        self.user_id = user_id
+        self.date_entered = date_entered
+        self.calories = calories
+        self.ingredient_id = ingredient_id
+
+@app.route('/seed')
+def initialize_database():
+    # Drop all existing tables (for testing purposes)
+    table_names = db.metadata.tables.keys()
+    print(', '.join(table_names))
+
+    db.drop_all()
+
+    # Create the tables
+    db.create_all()
+
+    # Create some example users
+    user1 = User(username='john', user_firstname="john", user_lastname="nee",
+                 user_password="pw")
+    user1weight = UserWeight(user_id="1", weight="80")
+    user1calories = UserNutrition(user_id = "1", date_entered=None,
+                                 calories="300",
+                                 ingredient_id="123")
+    user2 = User(username='mary', user_firstname="mary", user_lastname="mee",
+                 user_password="pw")
+    user2weight = UserWeight(user_id="2", weight="70")
+    user2calories = UserNutrition(user_id="2", date_entered=None,
+                                 calories="500",
+                                 ingredient_id="321")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    # Add the users to the session
+    db.session.add(user1)
+    db.session.add(user2)
+    db.session.add(user1weight)
+    db.session.add(user2weight)
+    db.session.add(user1calories)
+    db.session.add(user2calories)
+
+    # Commit the changes to the database
+    db.session.commit()
+
+    return 'Database initialized'
 
 
-# Press the green button in the gutter to run the script.
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = db.session.query(User, UserWeight, UserNutrition).\
+        join(UserWeight, User.user_id == UserWeight.user_id).\
+        join(UserNutrition, User.user_id == UserNutrition.user_id).all()
+
+    rows = []
+    for user, weight, nutrition in users:
+        user_data = {
+            'user_id': user.user_id,
+            'username': user.username,
+            'user_firstname': user.user_firstname,
+            'user_lastname': user.user_lastname,
+            'user_password': user.user_password,
+            'user_is_admin': user.user_is_admin,
+            'weight_id': weight.weight_id,
+            'weight': weight.weight,
+            'timestamp': weight.timestamp,
+            'nutrition_id': nutrition.nutrition_id,
+            'date_entered': nutrition.date_entered,
+            'calories': nutrition.calories,
+            'fat': nutrition.fat,
+            'saturated': nutrition.saturated,
+            'polyunsaturatedfat': nutrition.polyunsaturatedfat,
+            'monounsaturatedfat': nutrition.monounsaturatedfat,
+            'transfat': nutrition.transfat,
+            'cholesterol': nutrition.cholesterol,
+            'sodium': nutrition.sodium,
+            'potassium': nutrition.potassium,
+            'carbohydrates': nutrition.carbohydrates,
+            'fiber': nutrition.fiber,
+            'sugar': nutrition.sugar,
+            'protein': nutrition.protein
+        }
+        rows.append(user_data)
+
+    return jsonify({'users': rows})
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run()
